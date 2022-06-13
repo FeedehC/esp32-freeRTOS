58df6638e7c42640b5397ab2ca1abea04b3710af
FeedehC <feedehc@gmail.com>
2022-06-13 16:48:22 -0300

    feat: 4 LED blinking good

--- src/main.c
+++ src/main.c
@@ -4,23 +4,31 @@
 #include <freertos/task.h>
 
 #define LED 2 // LED connected to GPIO2
-#define DELAY 3000 // milliseconds to delay blink
+#define DELAY 1000 // milliseconds to delay blink
 
 int app_main() {
     // Configure pin
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL << LED);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
+    io_conf.pin_bit_mask = (1ULL << LED) | (1ULL << 18) | (1ULL << 21) | (1ULL << 22) | (1ULL << 23);
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&io_conf);
 
     // Main loop
     while(true) {
         gpio_set_level(LED, 0);
+        gpio_set_level(18, 0);
+        gpio_set_level(21, 0);
+        gpio_set_level(22, 0);
+        gpio_set_level(23, 0);
         vTaskDelay(pdMS_TO_TICKS(DELAY));
         gpio_set_level(LED, 1);
+        gpio_set_level(18, 1);
+        gpio_set_level(21, 1);
+        gpio_set_level(22, 1);
+        gpio_set_level(23, 1);
         vTaskDelay(pdMS_TO_TICKS(DELAY));
     }
 }
